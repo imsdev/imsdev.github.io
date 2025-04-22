166261b82d998a7bc4b4a2c8c37597aeca09d72e
Sarah Miller <hmillersarah@gmail.com>
2025-04-22 14:41:39 -0400

    clean up nav header js file

--- web-components/nav-header/nav-header.js
+++ web-components/nav-header/nav-header.js
@@ -12,19 +12,6 @@ function createComponent(html) {
     // Web component class representing a course card.
     class NavHeader extends HTMLElement {
 
-        // Creates an instance of CourseCard
-        constructor() {
-            super();
-        }
-
-        /**
-         * Returns an array of properties to observe.
-         * @returns {Array} An array of property names.
-        */
-        static get observedAttributes() {
-            return ['name'];
-        }
-
         /**
          * Called when an attribute is defined or changed.
          * @param {string} property - The name of the attribute.
@@ -42,116 +29,118 @@ function createComponent(html) {
             const shadow = this.attachShadow({mode: 'open'});
             shadow.innerHTML = html;
 
-            const menuButton = shadow.querySelector('.hamburger-menu'); 
-            const menuToggle = shadow.getElementById('menu-toggle');
-            const dropdownMenu = shadow.querySelector('.dropdown-menu');
-            const searchResults = shadow.getElementById('search-results');
-            const menuItems = dropdownMenu.querySelectorAll('a');
+            // Variables
+            // --------------------------------------------------------------------------------------------------
+                // Dropdown navigation menu
+                const menuButton = shadow.querySelector('.hamburger-menu'); 
+                const menuToggle = shadow.getElementById('menu-toggle'); 
+                // Search
+                const searchForm = shadow.getElementById('search-form'); 
+                const searchInput = shadow.getElementById('search-input'); 
+                const searchBar = shadow.querySelector('.search-bar'); 
+                const clearIcon = shadow.getElementById('clear-icon'); 
+                const searchResults = shadow.getElementById('search-results'); 
 
-            // Toggle dropdown menu on hamburger menu click
-            menuButton.addEventListener('click', () => {this.toggleMenu(menuToggle, dropdownMenu, menuItems)});
-            menuButton.addEventListener('keydown', (event) => {
-                if (event.key === 'Enter') {this.toggleMenu(menuToggle, dropdownMenu, menuItems)};
-            })
-                // Hide search results
+            // Event listeners for dropdown navigation menu
+            // --------------------------------------------------------------------------------------------------
+                // Toggle dropdown menu
+                menuButton.addEventListener('click', () => {this.toggleMenu()});
+                menuButton.addEventListener('keydown', (event) => {if (event.key === 'Enter') {this.toggleMenu()}});
+
+                // Hide search results if hamburger menu is focused
                 menuButton.addEventListener('focus', () => {searchResults.style.display = 'none';})
 
-            // Hide nav dropdowns when user clicks outside of menu
-            const contentBody = document.querySelector('#content');
-            contentBody.addEventListener('click', () => {
-                // Hide menu if open
-                if (menuToggle.checked) {
-                    this.toggleMenu(menuToggle, dropdownMenu, menuItems);
-                }
-                // Hide search results
-                searchResults.style.display = 'none';
-            })
+                // Hide dropdown menu for keyboard navigation when first link in document is focused
+                const firstLink = document.querySelector('a');
+                firstLink.addEventListener('focus', () => {if (menuToggle.checked) {this.toggleMenu()}});
 
-            // Focus on search
-            const searchInput = shadow.getElementById('search-input');
-            const searchBar = shadow.querySelector('.search-bar');
-            // If focus is on search
-            searchInput.addEventListener('focus', () => {
-                // Update search bar styling
-                searchBar.classList.add('search-bar-focused');
-                // Hide menu if open
-                if (menuToggle.checked) {
-                    this.toggleMenu(menuToggle, dropdownMenu, menuItems);
-                }
-                // Display search results
-                searchResults.style.display = 'block';
-            })
-            // If focus is off of search
-            searchInput.addEventListener('blur', () => {
-                // Update search bar styling
-                searchBar.classList.remove('search-bar-focused');
-            })
-            
-            // Clear search
-            const clearIcon = shadow.getElementById('clear-icon');
-            clearIcon.addEventListener('click', () => {this.clearSearch(searchInput, searchResults)});
-            clearIcon.addEventListener('keydown', (event) => {
-                if (event.key === 'Enter') {this.clearSearch(searchInput, searchResults)};
-            })
-            
-            // Search
-            const searchForm = shadow.getElementById('search-form');
-            // Await submission of search input
-            searchForm.addEventListener('submit', (event) => {
-                // Prevent page from reloading
-                event.preventDefault();
-                // Get user search input
-                const input = searchInput.value;
-                // Make search call
-                $(searchInput).tipuesearch(input, searchInput, searchResults, 'web-component');
-                // Get search results
-                const results = shadow.querySelectorAll('.tipue_search_content_title');
-                results.forEach(result => {
-                    result.addEventListener('click', this.handleResultClick.bind(this));
+            // Event listeners for all menus
+            // --------------------------------------------------------------------------------------------------
+                // Hide dropdowns when user clicks outside of search results or nav menu
+                const contentBody = document.querySelector('#content');
+                contentBody.addEventListener('click', () => {
+                    if (menuToggle.checked) {this.toggleMenu()};
+                    searchResults.style.display = 'none';
                 })
-                // Get dropdowns from search results
-                const accordions = shadow.querySelectorAll('.cordion');
-                accordions.forEach(accordion => {
-                    accordion.addEventListener('click', this.handleResultDropdown.bind(this));
-                })
-            });
 
-            // Hide dropdown menu for keyboard navigation
-            const firstLink = document.querySelector('a');
-            firstLink.addEventListener('focus', () => {
-                // Hide menu if open
-                if (menuToggle.checked) {
-                    this.toggleMenu(menuToggle, dropdownMenu, menuItems);
-                }
-            })
+            // Event listeners for search
+            // --------------------------------------------------------------------------------------------------
+                // On focus of search
+                searchInput.addEventListener('focus', () => {
+                    // Highlight search input field
+                    searchBar.classList.add('search-bar-focused');
+                    // Hide dropdown nav menu if open
+                    if (menuToggle.checked) {this.toggleMenu()};
+                    // Display search results
+                    searchResults.style.display = 'block';
+                })
+                // On blur of search
+                searchInput.addEventListener('blur', () => {searchBar.classList.remove('search-bar-focused')});
+            
+                // Clear search
+                clearIcon.addEventListener('click', () => {this.clearSearch()});
+                clearIcon.addEventListener('keydown', (event) => {if (event.key === 'Enter') {this.clearSearch()}});
+            
+                // Submission of search input
+                searchForm.addEventListener('submit', (event) => {
+                    // Prevent page from reloading
+                    event.preventDefault();
+                    // Get user search input
+                    const input = searchInput.value;
+                    // Make search call
+                    $(searchInput).tipuesearch(input, searchInput, searchResults, 'web-component');
+                    // Get search results
+                    const results = shadow.querySelectorAll('.tipue_search_content_title');
+                    // Clear search after result is clicked
+                    results.forEach(result => {
+                        result.addEventListener('click', this.clearSearch.bind(this));
+                    })
+                    // Get dropdowns from search results
+                    const accordions = shadow.querySelectorAll('.cordion');
+                    accordions.forEach(accordion => {
+                        accordion.addEventListener('click', this.handleResultDropdown.bind(this));
+                    })
+                });
         }
 
-        // Toggle dropdown menu
-        toggleMenu(menuToggle, dropdownMenu, menuItems) {
+        // Toggle dropdown navigation menu
+        toggleMenu() {
+            const component = document.querySelector('nav-header');
+            const shadow = component.shadowRoot;
+            const menuToggle = shadow.getElementById('menu-toggle'); 
+            const dropdownMenu = shadow.querySelector('.dropdown-menu'); 
+            const menuItems = dropdownMenu.querySelectorAll('a'); 
+
             // Update checked state of menuToggle
             var checkedState = menuToggle.checked ? false : true;
             menuToggle.checked = checkedState;
             // Update display of dropdown menu based on checked state
             if (checkedState) {
                 dropdownMenu.style.height = '35em';
+                // Make menu items focusable
                 for (let i = 0; i < menuItems.length; i++) {
                     menuItems[i].tabIndex = '0';
                 }
             } else {
                 dropdownMenu.style.height = '0em';
+                // Make menu items non-focusable
                 for (let i = 0; i < menuItems.length; i++) {
                     menuItems[i].tabIndex = '-1';
                 }
             }
         }
 
-        // Handle search result dropdown interaction
+        /**
+         * Handle search result dropdown interaction
+         * @param {event} event - User event
+        */
         handleResultDropdown(event) {
             const button = event.target;
             const parent = button.parentElement;
             const grandparent = parent.parentElement;
             var arrow;
             var description;
+
             // Check if user clicked on button - assign variables
             if (button.classList.contains('cordion')) {
                 arrow = button.firstChild;
@@ -166,24 +155,19 @@ function createComponent(html) {
             description.classList.toggle('search_content_drop');
         }
 
-        // Clear search after result is clicked
-        handleResultClick() {
+        // Clear search
+        clearSearch() {
             const component = document.querySelector('nav-header');
             const shadow = component.shadowRoot;
             const searchResults = shadow.getElementById('search-results');
             const searchInput = shadow.getElementById('search-input');
-            this.clearSearch(searchInput, searchResults);
-        }
 
-        // Clear search
-        clearSearch(searchInput, searchResults) {
             // Remove input value
             searchInput.value = "";
             const input = searchInput.value;
             // Make search call
             $(searchInput).tipuesearch(input, searchInput, searchResults, 'web-component');
             // Hide search results
-            // searchResults.classList.add('hide-results');
             searchResults.style.display = 'none';
         }
     }
