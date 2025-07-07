89c20e1451e29a3695284324c1ef67fb77520158
Sarah Miller <hmillersarah@gmail.com>
2025-07-07 15:45:14 -0400

    jump link web component setup

---
+++ web-components/jump-links/jump-links.js
@@ -0,0 +1,36 @@
+// Fetch HTML template
+fetch("web-components/jump-links/jump-links.html")
+    .then(stream => stream.text())
+    .then(text => createComponent(text))
+
+/**
+ * Creates a web component using a given HTML template.
+ * @param {string} html - The HTML template.
+ * @returns {void}
+ */
+function createComponent(html) {
+    // Web component class
+    class JumpLinks extends HTMLElement {
+
+        /**
+         * Called when an attribute is defined or changed.
+         * @param {string} property - The name of the attribute.
+         * @param {string} oldValue - The old value of the attribute.
+         * @param {string} newValue - The new value of the attribute.
+        */
+        attributeChangedCallback(property, oldValue, newValue) {
+            if (oldValue === newValue) return;
+            this[property] = newValue;
+        }
+    
+        // Invoked when element is added to document
+        connectedCallback() {
+            // Create shadow root for element
+            const shadow = this.attachShadow({mode: 'closed'});
+            shadow.innerHTML = html;
+        }
+    }
+
+    // Define new CourseCard element
+    customElements.define('jump-links', JumpLinks);
+}
